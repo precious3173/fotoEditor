12ed437ac46720f17f3631a17d576241fffe8bd5
Admin <preciouspeter3173@gmail.com>
2023-08-25 20:36:30 +0100

    dependency added

--- app/src/main/java/com/example/fotoeditor/MainActivity.kt
+++ app/src/main/java/com/example/fotoeditor/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.fotoeditor
 
 import android.os.Bundle
+import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,6 +12,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.example.fotoeditor.ui.theme.FotoEditorTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                 NavigationController()
                 }
             }
         }
@@ -30,14 +33,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NavigationController() {
+    val  navController = rememberNavController()
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     FotoEditorTheme {
-        Greeting("Android")
+       NavigationController()
     }
 }
