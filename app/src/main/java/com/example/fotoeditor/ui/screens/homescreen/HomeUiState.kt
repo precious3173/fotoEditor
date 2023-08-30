fec0abd91a97214e13ed372888a82c2d23180a06
khalidtouch <khalidhulabaloo9@gmail.com>
2023-08-30 02:45:14 +0100

    Implemented the import photo feature ---

---
+++ app/src/main/java/com/example/fotoeditor/ui/screens/homescreen/HomeUiState.kt
@@ -0,0 +1,13 @@
+package com.example.fotoeditor.ui.screens.homescreen
+
+
+import android.net.Uri
+
+data class HomeUiState(
+    val hasPhotoImported: Boolean = false,
+    val shouldExpandLooks: Boolean = hasPhotoImported,
+    val shouldExpandTools: Boolean = false,
+    val shouldShowExportMenu: Boolean = false,
+    val shouldShowOptionsMenu: Boolean = false,
+    var importedImageUri: Uri? = null,
+)
